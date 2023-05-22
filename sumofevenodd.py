6d7899c5759fc9204ded7cdd62ab4fff4634737b
nikhitkumar <nikhitkumar00@gmail.com>
2023-05-22 15:21:25 +0530

    added sum of odd even

---
+++ sumofevenodd.py
@@ -0,0 +1,18 @@
+def sumOfEvenOdd(num):
+
+    sume=0
+    sumo=0
+    while(num>0):
+        if((num%10)%2 == 0):
+            sume += (num%10)
+        else:
+            sumo += (num%10)
+        num = num//10
+    return (sume,sumo)
+
+def main():
+    x = sumOfEvenOdd(1234)
+    print(x)
+
+if __name__ == "__main__":
+    main()
