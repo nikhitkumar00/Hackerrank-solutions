060c3b10f44023cd83543b4f95df51d0881b1676
nikhitkumar <nikhitkumar00@gmail.com>
2023-05-24 15:10:47 +0530

    added dec to roman

---
+++ dec to roman.py
@@ -0,0 +1,17 @@
+def DEC_ROM(n):
+    symbols = [
+        ("M", 1000), ("CM", 900), ("D", 500), ("CD", 400),
+        ("C", 100), ("XC", 90), ("L", 50), ("XL", 40),
+        ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I", 1)
+    ]
+    
+    roman = "" 
+    
+    for symbol, value in symbols:
+        while n >= value:
+            roman += symbol
+            n -= value
+    
+    return roman
+
+print(DEC_ROM(int(input())))
--- hcflcm.py
+++ hcflcm.py
@@ -1,8 +1,4 @@
-# n = int(input())
-# l = [input().split()]
-
-
-def gcd(l):
+def ggcd(l):
     large = max(l)
     for i in range(large, 0, -1):
         flag = 0
@@ -13,10 +9,14 @@ def gcd(l):
         if flag == 0:
             return i
 
-l = [184,230,276]
-hcf = gcd(l)
+n = int(input())
+m = input().split()
+l = []
+for i in m:
+    l.append(int(i))
+hcf = ggcd(l)
 product = 1
 for i in l:
-    product = (product * i)//gcd([product,i])
+    product = (product * i)//ggcd([product,i])
 lcm = product
-print(hcf,lcm)
+print(f"HCF={hcf} LCM={lcm}")
