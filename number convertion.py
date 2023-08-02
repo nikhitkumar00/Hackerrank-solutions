ac7ba60de56b3c1624d371992f38d1a83d1a89ee
nikhitkumar <nikhitkumar00@gmail.com>
2023-08-02 10:16:26 +0530

    added conversion

---
+++ number convertion.py
@@ -0,0 +1,17 @@
+a = "100"
+
+print(int(a,2))     # Binary
+print(int(a,8))     # Octal
+print(int(a,16))    # Hexadecimal
+
+a = 100
+
+print(bin(a))       # Decimal to Binary
+print(oct(a))       # Decimal to Octal
+print(hex(a))       # Decimal to Hexadecimal
+
+a = 100
+
+print(bin(a)[2:])   # After Slicing
+print(oct(a)[2:])   # After Slicing
+print(hex(a)[2:])   # After Slicing
