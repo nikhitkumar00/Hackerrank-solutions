8be76def7210f1ebf1b927d375c309c545d32cd0
nikhitkumar <nikhitkumar00@gmail.com>
2023-08-02 09:52:09 +0530

    added logical vs bitwise

---
+++ logical and bitwise.py
@@ -0,0 +1,14 @@
+a = 10
+b = 6
+c = 0
+
+print(a and b) # most recent true result will be displayed
+print(a & b)
+
+print(a and b and c)
+
+"""
+    here a is 10 so true so result will be 10
+    but when b is 6 and is true it will be 6
+    hence most recent true value will be displayed
+"""
