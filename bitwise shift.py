591c6de0ce0d6264fff842ef1bdab8089697e8c8
nikhitkumar <nikhitkumar00@gmail.com>
2023-08-02 10:01:08 +0530

    added shift

--- bitwise shift.py
+++ bitwise shift.py
@@ -1,7 +1,16 @@
 a = 6
+
 print(a << 1)   # left shift by 1 position
+print(a >> 1)   # right shift by 1 position
+
 """
     0 1 1 0     6(decimal)
     -------
     1 1 0 0     12(decimal)
+
+--------------------------------------
+
+    0 1 1 0     6(decimal)
+    -------
+    0 0 1 1     3(decimal)
 """
