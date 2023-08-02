4817f1aa23a9fd88fc0b19732197d72d59c57745
nikhitkumar <nikhitkumar00@gmail.com>
2023-08-02 09:31:34 +0530

    added bitwse or

---
+++ biitwise.py
@@ -0,0 +1,9 @@
+a = 3
+b = 2
+c = 1
+
+d = a | b & c
+print(d)
+
+d = a | b & ~c
+print(d)
---
+++ bitwise or.py
@@ -0,0 +1,11 @@
+x = 9 # Binary 1001
+y = 3 # Binary 0110
+result = x | y
+print(result) 
+
+"""
+        1 0 0 1
+        0 1 1 0
+    ---------------
+        1 1 1 1  === 11 (decimal)
+"""
