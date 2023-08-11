8be2318c41d983b5d320dc9fd2d93980cba4e730
nikhitkumar <nikhitkumar00@gmail.com>
2023-08-11 20:49:29 +0530

    done

--- stack-4.py
+++ stack-4.py
@@ -3,21 +3,22 @@
 """
 
 
+def islessprecedence(p, q):
+    dict = {"(": 0, "=": 1, "+": 2, "-": 2, "*": 3, "/": 3, "^": 4}
+    a = dict[p]
+    b = dict[q]
+
+    if p == "^" and q == "^":
+        return False
+    elif a <= b:
+        return True
+    else:
+        return False
+
+
 def infixtopostfix(s):
     stack = []
     out = ""
-
-    def islessprecedence(p, q):
-        dict = {"(": 0, "=": 1, "+": 2, "-": 2, "*": 3, "/": 3, "^": 4}
-        a = dict[p]
-        b = dict[q]
-
-        if p == "^" and q == "^":
-            return False
-        elif a <= b:
-            return True
-        else:
-            return False
 
     for ele in s:
         if ele.isalpha():
