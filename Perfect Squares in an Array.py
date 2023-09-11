a1bb12385b306c896ca993c91eef9aaf2417fb83
nikhitkumar00 <nikhitkumar00@gmail.com>
2023-09-11 22:35:01 +0530

    done

---
+++ Perfect Squares in an Array.py
@@ -0,0 +1,14 @@
+def checkPerfectSquare(ob, N):
+    if N < 0:
+        return 0
+
+    i = 1
+    while N > 0:
+        N -= i
+        i += 2
+
+    return 1 if N == 0 else 0
+
+
+N = int(input())
+print(checkPerfectSquare(N))
