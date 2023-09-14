68fe64620307f1c3d61d150dfbdf2ce9c101d0ff
nikhitkumar00 <nikhitkumar00@gmail.com>
2023-09-14 13:51:56 +0530

    done

--- remove even and duplicate elements.py
+++ remove even and duplicate elements.py
@@ -1,16 +1,9 @@
 l = [1, 1, 2, 2, 3, 3, 4, 5, 4, 5, 6, 7, 89, 44, 10]
 
-dic = {}
-try:
-    i = 0
-    while 1:
-        if l[i] % 2 == 0:
-            l.pop(i)
-        else:
-            if l[i] in dic:
-                l.pop(i)
-            else:
-                dic[l[i]] = 1
-                i += 1
-except:
-    print(l)
+s = set(l)
+out = 0
+for i in s:
+    if i % 2 != 0:
+        out += 1
+
+print(out)
